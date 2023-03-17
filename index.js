9c722baa91201374fc584fc829293a7817f6a439
Antonis Stamatiou <stamatiou.antonis@gmail.com>
2023-03-17 14:16:44 +0200

    test both scenarios

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const { request } = require("@octokit/request");
 const { core } = require('@actions/core');
 
-const result = await request('GET /repositories/{repository_id}/environments/{environment_name}/variables', {
-  headers: {
-    authorization: "token ${{ secrets.TEMP_PAT }}",
-  },
-  repository_id: '${{ github.repository_id }}',
-  environment_name: 'test',
-});
+const run = async function run() {
 
-console.log(`${result.total_count} variables found`)
+  const result = await request('GET /repositories/{repository_id}/environments/{environment_name}/variables', {
+    headers: {
+      authorization: "token " + process.env.TOKEN,
+    },
+    repository_id: process.env.REPOSITORY_ID,
+    environment_name: process.env.ENVIRONMENT,
+  });
+  
+  console.log(`${result.data.total_count} variables found`)
+  
+  result.data.variables.forEach(function (item) {
+    core.exportVariable(item['name'], item['value']);
+  });
 
-result.variables.forEach(function (item, index) {
-  core.exportVariable(item['name'], item['value']);
-});
+}
+
+run();
